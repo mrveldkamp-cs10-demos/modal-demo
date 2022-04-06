d2c985e714435cc448705618980f31a9f89ea250
mrveldkamp <colin.veldkamp@epsb.ca>
2022-04-06 14:23:07 -0600

    simple demo working

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log("hi");
+// Simple Modal Demo
+
+// HTML Variables
+let showModalBtnEl = document.getElementById("show-modal-btn");
+let hideModalBtnEl = document.getElementById("hide-modal-btn");
+let modalEl = document.getElementById("modal");
+
+// Event Listener
+showModalBtnEl.addEventListener("click", showModal);
+hideModalBtnEl.addEventListener("click", hideModal);
+modalEl.addEventListener("click", checkModalClose);
+
+// Event Functions
+function showModal() {
+  modalEl.style.display = "block";
+}
+
+function hideModal() {
+  modalEl.style.display = "none";
+}
+
+function checkModalClose(e) {
+  // Close Modal if outer area clicked
+  if (e.target === modalEl) {
+    modalEl.style.display = "none";
+  }
+}
